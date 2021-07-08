8f34186445ded599780429669b0fc1b9bddbf7af
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-07-08 17:12:43 +0200

    Added route alias

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
 
 //Components
-import Dialog from "../components/Dialog";
+import Dialog from "@/components/Dialog";
 
 Vue.use(VueRouter)
 
